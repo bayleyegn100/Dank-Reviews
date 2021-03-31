f599819ce1c641cb0d59f500e53003dc55c84f5f
Jesse Willig <jessewillig@outlook.com>
2021-03-30 20:00:46 -0700

    completed homeRoutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -45,6 +45,31 @@ router.get('/review/:id', async (req, res) => {
     }
 });
 
-router.get
+router.get('/profile', withAuth, async (req, res) => {
+    try {
+        const userData = await User.findByPk(req.session.user_id, {
+            attributes: { exclude: ['password'] },
+            include: [{ model: Review }]
+        });
+        
+        const user = userData.get({ plain: true });
+
+        res.render('profile', {
+            ...user,
+            logged_in: true
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/login', (req, res) => {
+    if (req.session.logged_in) {
+        res.redirect('/profile');
+        return;
+    }
+
+    res.render('login');
+});
 
 module.exports = router;
