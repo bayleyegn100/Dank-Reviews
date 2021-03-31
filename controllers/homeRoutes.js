058146b8f8f2e828426e0cdc9fcdf66ce0d98632
Jesse Willig <jessewillig@outlook.com>
2021-03-30 19:51:38 -0700

    working on homeRoutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,1 +1,50 @@
-const express = require('express').Router();
+const router = require('express').Router();
+const { User, Review } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', async (req, res) => {
+    try {
+        const reviewData = await Review.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['name']
+                },
+            ],
+        });
+
+        const reviews = reviewData.map((review) => review.get({ plain: true }));
+
+        res.render('homepage', {
+            reviews,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/review/:id', async (req, res) => {
+    try {
+        const reviewData = await Review.findByPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    attributes: ['name']
+                },
+            ],
+        });
+        const review = reviewData.get({ plain: true });
+
+        res.render('review', {
+            ...review,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get
+
+module.exports = router;
