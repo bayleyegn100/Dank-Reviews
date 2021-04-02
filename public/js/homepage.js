e7fee35cbc64e4596a4e70df9afb5bab1ebed07a
Jesse Willig <jessewillig@outlook.com>
2021-04-01 20:16:16 -0700

    started the homepage.js functionality

---
+++ public/js/homepage.js
@@ -0,0 +1,21 @@
+const newReview = async = function (event) {
+    event.preventDefault();
+
+    const newReview = document.querySelector('#review').nodeValue.trim();
+
+    if (newReview) {
+        const response = await fetch(`/api/reviews`, {
+            method: 'POST',
+            body: JSON.stringify({ newReview }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        if (response.ok) {
+            document.location.replace('/homepage');
+        } else {
+            alert(response.statusText);
+        }
+    }
+};
+
+document.querySelector('#review').addEventListener('submit', newReview);
