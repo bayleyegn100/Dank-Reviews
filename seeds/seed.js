37bc9ffdc81c37b4484d3e8d42f46756aafb0b1b
Haram Kwon <kharam1436@gmail.com>
2021-04-01 19:26:52 -0700

    seeds now populate image

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,8 +1,9 @@
 const sequelize = require('../config/connection');
-const { User, Review } = require('../models');
+const { User, Review, Image } = require('../models');
 
 const userData = require('./userData.json');
 const reviewData = require('./reviewData.json');
+const imageData = require('./imageData.json')
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -12,10 +13,13 @@ const seedDatabase = async () => {
     returning: true,
   });
 
+  const images = await Image.bulkCreate(imageData)
+
   for (const review of reviewData) {
     await Review.create({
       ...review,
       user_id: users[Math.floor(Math.random() * users.length)].id,
+      image_id: images[Math.floor(Math.random() * images.length)].id
     });
   }
 
