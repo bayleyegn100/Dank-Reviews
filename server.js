554277f671be8897932bccd382a4bc3ac1894a35
bayleyegn100 <bayleyegn100@gmail.com>
2021-04-24 09:47:37 -0700

    trying to match the local and remote repo

--- server.js
+++ server.js
@@ -4,11 +4,12 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
-
+var compression = require('compression')
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
+app.use(compression());
 const PORT = process.env.PORT || 3001;
 
 const hbs = exphbs.create({ helpers });
@@ -24,6 +25,7 @@ const sess = {
 };
 
 app.use(session(sess));
+
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
